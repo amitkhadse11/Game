d6a46d1a061d8625117ff13bfa3f50d2a9bb0185
amit kahdse <amitkhadse011@gmail.com>
2024-11-22 12:53:02 +0530

    Step two of the game

--- Number Gussing Game/src/com/velocity/number.java
+++ Number Gussing Game/src/com/velocity/number.java
@@ -3,5 +3,6 @@ package com.velocity;
 public class number {
 public static void main(String[] args) {
 	System.out.println("welcome to number gussing game");
+	System.out.println("enter the number");
 }
 }
